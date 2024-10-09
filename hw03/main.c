e4c960057d37be0754abff7af370b28d2ef3b29f
Seungpyo Hong <spkbk98@gmail.com>
2024-10-09 11:15:39 +0900

    all equations solved

--- hw03/main.c
+++ hw03/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "nr.h"
 #include "nrutil.h"
@@ -41,138 +42,183 @@ void report(const char *method, float *sol, int n) {
     printf("]\n");
 }
 
-int main() {
-    char fname[32];
-    for (int file_idx = 1; file_idx <= 3; ++file_idx) {
-        sprintf(fname, "lineq_dat/lineq%d.dat", file_idx);
-        FILE *fp = fopen(fname, "r");
-        if (fp == NULL) {
-            fprintf(stderr, "Cannot open file %s\n", fname);
-            return 1;
+void gauss_jordan(float **a, float **b, int n) {
+    gaussj(a, n, b, 1);
+    float *gauss_x = vector(1, n);
+    flatten(b, gauss_x, 1, n);
+    report("Gauss-Jordan", gauss_x, n);
+    free_vector(gauss_x, 1, n);
+}
+
+void lu_decomposition(float **a, float **b, int n) {
+    float *bvec = vector(1, n);
+    int *ivec = ivector(1, n);
+    float d;
+    flatten(b, bvec, 1, n);
+    ludcmp(a, n, ivec, &d);
+    lubksb(a, n, ivec, bvec);
+    report("LU Decomposition", bvec, n);
+    free_vector(ivec, 1, n);
+}
+
+void svd(float **a, float **b, int n) {
+    float *w = vector(1, n);
+    float **v = matrix(1, n, 1, n);
+    float *x = vector(1, n);
+    float *bvec = vector(1, n);
+    flatten(b, bvec, 1, n);
+
+    svdcmp(a, n, n, w, v);
+
+    float maxSingular = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (w[i] > maxSingular) {
+            maxSingular = w[i];
         }
-        printf("\nFile: %s\n", fname);
-        int m, n;
-        fscanf(fp, "%d %d", &m, &n);
-        float **a = matrix(1, m, 1, n);
-        float **b_mat = matrix(1, m, 1, 1);
-        float *b = vector(1, m);
-        float **c = matrix(1, n, 1, 1);
-        float d;
-        float *w = vector(1, n);
-        float **v = matrix(1, n, 1, n);
-        float *x = vector(1, n);
-        int *ivec = ivector(1, n);
-        float **a_bkup = matrix(1, m, 1, n);
-        float **b_bkup = matrix(1, m, 1, 1);
-
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                fscanf(fp, "%f", &a[i][j]);
-            }
-        }
-        for (int i = 1; i <= m; ++i) {
-            fscanf(fp, "%f", &b_mat[i][1]);
-        }
-        fclose(fp);
-
-        copy_matrix(a, a_bkup, 1, m, 1, n);
-        copy_matrix(b_mat, b_bkup, 1, m, 1, 1);
-
-        ////////// Gauss-Jordan
-        gaussj(a, n, b_mat, 1);
-        printf("Method: Gauss-Jordan, Solution: [");
-        float **gauss_x = vector(1, n);
-        flatten(b_mat, gauss_x, 1, n);
-        report("Gauss-Jordan", gauss_x, n);
-        free_vector(gauss_x, 1, n);
-
-        copy_matrix(a_bkup, a, 1, m, 1, n);
-        copy_matrix(b_bkup, b_mat, 1, m, 1, 1);
-
-        flatten(b_mat, b, 1, m);
-        ludcmp(a, m, ivec, &d);
-        lubksb(a, m, ivec, b);
-        report("LU Decomposition", b, n);
-        printf("]\n");
-
-        copy_matrix(a_bkup, a, 1, m, 1, n);
-        copy_matrix(b_bkup, b_mat, 1, m, 1, 1);
-
-        /////////////////// SVD
-        flatten(b_mat, b, 1, m);
-        svdcmp(a, m, n, w, v);
-        float maxSingular = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (w[i] > maxSingular) {
-                maxSingular = w[i];
-            }
-        }
-        for (int i = 1; i <= n; ++i) {
-            if (w[i] < 1e-6 * maxSingular) {
-                w[i] = 0;
-            }
-        }
-        svbksb(a, w, v, m, n, b, x);
-        report("SVD", x, n);
-
-        /////////////// improved LU decomposition
-
-        copy_matrix(a_bkup, a, 1, m, 1, n);
-        copy_matrix(b_bkup, b_mat, 1, m, 1, 1);
-        flatten(b_mat, b, 1, m);
-        ludcmp(a, m, ivec, &d);
-        lubksb(a, m, ivec, b);
-        report("mprove, LU Decomposition Only", x, n);
-        // Add random noise to b
-        for (int i = 1; i <= m; ++i) {
-            x[i] += ((float)rand() / (float)RAND_MAX) * 0.01;
-        }
-        report("mprove (noisy)", x, n);
-        float **a_mprove = matrix(1, m, 1, n);
-        copy_matrix(a_bkup, a_mprove, 1, m, 1, n);
-        mprove(a_mprove, a, n, ivec, b, x);
-        report("mprove (improved LU)", x, n);
-        free_matrix(a_mprove, 1, m, 1, n);
-
-        /////////// Inverse Matrix
-        copy_matrix(a_bkup, a, 1, m, 1, n);
-        ludcmp(a, m, ivec, &d);
-
-        float *col = vector(1, m);
-        float **y = matrix(1, m, 1, n);
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                col[j] = 0;
-            }
-            col[i] = 1;
-            lubksb(a, m, ivec, col);
-            for (int j = 1; j <= m; ++j) {
-                y[j][i] = col[j];
-            }
-        }
-        printf("LU Decomposition, Inverse Matrix:\n");
-        print_mat(y, 1, m, 1, n);
-        free_vector(col, 1, m);
-        free_matrix(y, 1, m, 1, n);
-
-        ///////////// Determinant
-        ludcmp(a, m, ivec, &d);
-        for (int i = 1; i <= n; ++i) {
-            d *= a[i][i];
-        }
-        printf("LU Decomposition, Determinant: %f\n", d);
-
-        ///////////// Clean up bye bye
-        free_ivector(ivec, 1, n);
-        free_vector(b, 1, m);
-        free_matrix(a, 1, m, 1, n);
-        free_matrix(b_mat, 1, m, 1, 1);
-        free_matrix(c, 1, n, 1, 1);
-        free_matrix(a_bkup, 1, m, 1, n);
-        free_matrix(b_bkup, 1, m, 1, 1);
-        free_vector(w, 1, n);
-        free_matrix(v, 1, n, 1, n);
-        free_vector(x, 1, n);
     }
+    for (int i = 1; i <= n; ++i) {
+        if (w[i] < 1e-6 * maxSingular) {
+            w[i] = 0;
+        }
+    }
+
+    svbksb(a, w, v, n, n, b, x);
+    report("SVD", x, n);
+
+    free_vector(w, 1, n);
+    free_matrix(v, 1, n, 1, n);
+    free_vector(x, 1, n);
+    free_vector(bvec, 1, n);
+}
+
+void improved_lu_decomposition(float **a, float **b, int n) {
+    int *ivec = ivector(1, n);
+    float *bvec = vector(1, n);
+    float *x = vector(1, n);
+    float **a_mprove = matrix(1, n, 1, n);
+    float d;
+
+    copy_matrix(a, a_mprove, 1, n, 1, n);
+    flatten(b, x, 1, n);
+    flatten(b, bvec, 1, n);
+
+    ludcmp(a, n, ivec, &d);
+    lubksb(a, n, ivec, x);
+    report("Improved LU Decomposition", x, n);
+
+    for (int i = 1; i <= n; ++i) {
+        x[i] += ((float)rand() / (float)RAND_MAX);
+    }
+    report("Improved LU Decomposition (noisy)", x, n);
+
+    mprove(a_mprove, a, n, ivec, bvec, x);
+    report("Improved LU Decomposition (mprove)", x, n);
+
+    free_vector(bvec, 1, n);
+    free_ivector(ivec, 1, n);
+    free_vector(x, 1, n);
+    free_matrix(a_mprove, 1, n, 1, n);
+}
+
+void inverse_matrix(float **a, int n) {
+    int *ivec = ivector(1, n);
+    float *col = vector(1, n);
+    float **y = matrix(1, n, 1, n);
+    float d;
+
+    ludcmp(a, n, ivec, &d);
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            col[j] = i == j ? 1 : 0;
+        }
+        lubksb(a, n, ivec, col);
+        for (int j = 1; j <= n; ++j) {
+            y[j][i] = col[j];
+        }
+    }
+    printf("Inverse Matrix:\n");
+    print_mat(y, 1, n, 1, n);
+
+    free_ivector(ivec, 1, n);
+    free_vector(col, 1, n);
+    free_matrix(y, 1, n, 1, n);
+}
+
+void determinant(float **a, int n) {
+    int *ivec = ivector(1, n);
+    float d;
+    ludcmp(a, n, ivec, &d);
+    for (int i = 1; i <= n; ++i) {
+        d *= a[i][i];
+    }
+    printf("Determinant: %f\n", d);
+    free_ivector(ivec, 1, n);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    char fname[32];
+    strcpy(fname, argv[1]);
+    FILE *fp = fopen(fname, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Cannot open file %s\n", fname);
+        return 1;
+    }
+    printf("\nFile: %s\n", fname);
+    int m, n;
+    fscanf(fp, "%d %d", &m, &n);
+    float **a = matrix(1, m, 1, n);
+    float **b = matrix(1, m, 1, 1);
+    float **a_bkup = matrix(1, m, 1, n);
+    float **b_bkup = matrix(1, m, 1, 1);
+
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            fscanf(fp, "%f", &a_bkup[i][j]);
+        }
+    }
+    for (int i = 1; i <= m; ++i) {
+        fscanf(fp, "%f", &b_bkup[i][1]);
+    }
+    fclose(fp);
+
+    // Gauss-Jordan
+    copy_matrix(a_bkup, a, 1, m, 1, n);
+    copy_matrix(b_bkup, b, 1, m, 1, 1);
+    gauss_jordan(a, b, n);
+
+    // LU Decomposition
+    copy_matrix(a_bkup, a, 1, m, 1, n);
+    copy_matrix(b_bkup, b, 1, m, 1, 1);
+    lu_decomposition(a, b, n);
+
+    // SVD
+    copy_matrix(a_bkup, a, 1, m, 1, n);
+    copy_matrix(b_bkup, b, 1, m, 1, 1);
+    svd(a, b, n);
+
+    // improved LU decomposition
+    copy_matrix(a_bkup, a, 1, m, 1, n);
+    copy_matrix(b_bkup, b, 1, m, 1, 1);
+    improved_lu_decomposition(a, b, n);
+
+    /////////// Inverse Matrix
+    copy_matrix(a_bkup, a, 1, m, 1, n);
+    inverse_matrix(a, n);
+
+    ///////////// Determinant
+    copy_matrix(a_bkup, a, 1, m, 1, n);
+    determinant(a, n);
+
+    ///////////// Clean up bye bye
+    free_matrix(a, 1, m, 1, n);
+    free_matrix(b, 1, m, 1, 1);
+    free_matrix(a_bkup, 1, m, 1, n);
+    free_matrix(b_bkup, 1, m, 1, 1);
+
     return 0;
 }
